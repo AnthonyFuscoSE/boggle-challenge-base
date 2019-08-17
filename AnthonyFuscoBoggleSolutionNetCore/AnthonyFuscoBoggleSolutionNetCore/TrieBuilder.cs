c2aa477e4a94a616509fba421997c21332c45c3e
Anthony Fusco <54122963+AnthonyFuscoSE@users.noreply.github.com>
2019-08-17 09:24:28 -0600

    Update TrieBuilder.cs
    Removed duplicate using

--- AnthonyFuscoBoggleSolutionNetCore/AnthonyFuscoBoggleSolutionNetCore/TrieBuilder.cs
+++ AnthonyFuscoBoggleSolutionNetCore/AnthonyFuscoBoggleSolutionNetCore/TrieBuilder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using AnthonyFuscoBoggleSolutionNetCore.Data;
 using AnthonyFuscoBoggleSolutionNetCore.Models;
-using AnthonyFuscoBoggleSolutionNetCore.Models;
 
 namespace AnthonyFuscoBoggleSolutionNetCore
 {
